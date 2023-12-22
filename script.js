fb65abec220373d9eb78c4b0560bac87676924f3
Alex Quatro <dp050280sov@gmail.com>
2023-12-22 19:41:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,214 @@
 'use strict';
 
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('input', highlightAndFocus);
+
+let originalContent = '';
+let highlightedElements = [];
+let currentHighlightedIndex = -1;
+
+function highlightAndFocus() {
+	const searchText = searchInput.value.trim();
+	const contentNode = document.getElementById('content');
+	const lowerCaseSearchText = searchText.toLowerCase();
+
+	if (!originalContent) {
+		originalContent = contentNode.innerHTML;
+	}
+
+	const spanElement = document.createElement('span');
+	spanElement.className = 'highlight';
+
+	contentNode.innerHTML = originalContent;
+
+	if (searchText !== '') {
+		contentNode.innerHTML = contentNode.innerHTML.replace(new RegExp(lowerCaseSearchText, 'gi'), matchedText => {
+				const newSpanElement = spanElement.cloneNode(true);
+				newSpanElement.textContent = matchedText;
+				return newSpanElement.outerHTML;
+		});
+
+		// Оновлюємо масив виділених елементів
+		highlightedElements = Array.from(contentNode.querySelectorAll('.highlight'));
+
+		// Знайдемо перший виділений елемент
+		const firstHighlightedElement = highlightedElements[0];
+
+		if (firstHighlightedElement) {
+				// Фокусуємося на першому виділеному елементі
+				firstHighlightedElement.focus();
+
+				// Прокручуємо так, щоб елемент був посередині вікна за висотою
+				const windowHeight = window.innerHeight;
+				const elementTop = firstHighlightedElement.offsetTop;
+				const elementHeight = firstHighlightedElement.offsetHeight;
+
+				const scrollTo = elementTop - (windowHeight - elementHeight) / 2;
+
+				window.scrollTo({
+					top: scrollTo,
+					behavior: 'smooth',
+				});
+
+				// Встановлюємо поточний індекс виділеного елемента
+				currentHighlightedIndex = 0;
+		}
+	}
+}
+
+function preventDefaultAndNextResult(event) {
+	event.preventDefault();
+	nextResult();
+}
+
+function preventDefaultAndPreviousResult(event) {
+	event.preventDefault();
+	previousResult();
+}
+
+function scrollIntoView(element) {
+	const windowHeight = window.innerHeight;
+	const elementTop = element.offsetTop;
+	const elementHeight = element.offsetHeight;
+
+	const scrollTo = elementTop - (windowHeight - elementHeight) / 2;
+
+	window.scrollTo({
+		top: scrollTo,
+		behavior: 'smooth',
+	});
+}
+
+function nextResult() {
+	if (highlightedElements.length > 0 && currentHighlightedIndex < highlightedElements.length - 1) {
+		// Збільшуємо поточний індекс і фокусуємося на наступному елементі
+		currentHighlightedIndex++;
+		highlightedElements[currentHighlightedIndex].focus();
+
+		// Прокручуємо до середини вікна за висотою
+		scrollIntoView(highlightedElements[currentHighlightedIndex]);
+	}
+}
+
+function previousResult() {
+	if (highlightedElements.length > 0 && currentHighlightedIndex > 0) {
+		// Зменшуємо поточний індекс і фокусуємося на попередньому елементі
+		currentHighlightedIndex--;
+		highlightedElements[currentHighlightedIndex].focus();
+		
+		// Прокручуємо до середини вікна за висотою
+		scrollIntoView(highlightedElements[currentHighlightedIndex]);
+	}
+}
+
+const nextButton = document.getElementById('nextButton');
+nextButton.addEventListener('click', preventDefaultAndNextResult);
+
+const prevButton = document.getElementById('prevButton');
+prevButton.addEventListener('click', preventDefaultAndPreviousResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const searchInput = document.getElementById('searchInput');
+// searchInput.addEventListener('input', highlightAndFocus);
+
+// let originalContent = '';
+
+// function highlightAndFocus() {
+//     const searchText = searchInput.value.trim();
+//     const contentNode = document.getElementById('content');
+//     const lowerCaseSearchText = searchText.toLowerCase();
+
+//     if (!originalContent) {
+//         originalContent = contentNode.innerHTML;
+//     }
+
+//     const spanElement = document.createElement('span');
+//     spanElement.className = 'highlight';
+
+//     contentNode.innerHTML = originalContent;
+
+//     if (searchText !== '') {
+//         contentNode.innerHTML = contentNode.innerHTML.replace(new RegExp(lowerCaseSearchText, 'gi'), matchedText => {
+//             const newSpanElement = spanElement.cloneNode(true);
+//             newSpanElement.textContent = matchedText;
+//             return newSpanElement.outerHTML;
+//         });
+
+//         // Знайдемо перший виділений елемент
+//         const firstHighlightedElement = contentNode.querySelector('.highlight');
+
+//         if (firstHighlightedElement) {
+//             // Фокусуємося на першому виділеному елементі
+//             firstHighlightedElement.focus();
+
+//             // Прокручуємо так, щоб елемент був посередині вікна за висотою
+//             const windowHeight = window.innerHeight;
+//             const elementTop = firstHighlightedElement.offsetTop;
+//             const elementHeight = firstHighlightedElement.offsetHeight;
+
+//             const scrollTo = elementTop - (windowHeight - elementHeight) / 2;
+
+//             window.scrollTo({
+//                 top: scrollTo,
+//                 behavior: 'smooth',
+//             });
+//         }
+//     }
+// }
+
+// function preventDefaultAndNextResult(event) {
+//     event.preventDefault();
+//     nextResult();
+// }
+
+// function preventDefaultAndPreviousResult(event) {
+//     event.preventDefault();
+//     previousResult();
+// }
+
+// function nextResult() {
+//     // ваш код
+// }
+
+// function previousResult() {
+//     // ваш код
+// }
+
+// const nextButton = document.getElementById('nextButton');
+// nextButton.addEventListener('click', preventDefaultAndNextResult);
+
+// const prevButton = document.getElementById('prevButton');
+// prevButton.addEventListener('click', preventDefaultAndPreviousResult);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // function search() {
 // 	const searchText = document.getElementById('searchInput').value.trim();
 // 	const contentText = document.getElementById('content').innerText;
@@ -27,6 +236,10 @@
 // 	}
 // }
 
+
+
+
+
 // const searchInput = document.getElementById('searchInput');
 // searchInput.addEventListener('input', search);
 
@@ -34,32 +247,31 @@
 
 // searchButton.addEventListener('click', search);
 
-let originalContent = '';
+// let originalContent = '';
 
-function highlightText() {
-	const searchText = document.getElementById('searchInput').value.trim();
-	const contentNode = document.getElementById('content');
-	const lowerCaseSearchText = searchText.toLowerCase();
+// function highlightText() {
+// 	const searchText = document.getElementById('searchInput').value.trim();
+// 	const contentNode = document.getElementById('content');
+// 	const lowerCaseSearchText = searchText.toLowerCase();
 
-	if (!originalContent) {
-		originalContent = contentNode.innerHTML;
-	}
+// 	if (!originalContent) {
+// 		originalContent = contentNode.innerHTML;
+// 	}
 
-	const spanElement = document.createElement('span');
-	spanElement.className = 'highlight';
+// 	const spanElement = document.createElement('span');
+// 	spanElement.className = 'highlight';
 
-	contentNode.innerHTML = originalContent;
+// 	contentNode.innerHTML = originalContent;
 
-	if (searchText !== '') {
-		contentNode.innerHTML = contentNode.innerHTML.replace(new RegExp(lowerCaseSearchText, 'gi'), matchedText => {
-				const newSpanElement = spanElement.cloneNode(true);
-				newSpanElement.textContent = matchedText;
-				return newSpanElement.outerHTML;
-		});
-	}
-}
+// 	if (searchText !== '') {
+// 		contentNode.innerHTML = contentNode.innerHTML.replace(new RegExp(lowerCaseSearchText, 'gi'), matchedText => {
+// 				const newSpanElement = spanElement.cloneNode(true);
+// 				newSpanElement.textContent = matchedText;
+// 				return newSpanElement.outerHTML;
+// 		});
+// 	}
+// }
 
-const searchInput = document.getElementById('searchInput');
-searchInput.addEventListener('input', highlightText);
-
+// const searchInput = document.getElementById('searchInput');
+// searchInput.addEventListener('input', highlightText);
 
